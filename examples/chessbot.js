48398b0bbe3da0ab2a9bddf66a532d569c2aa426
Zachary Capalbo <zach.geek@gmail.com>
2020-05-16 20:05:42 -0400

    Working chessbot

--- examples/chessbot.js
+++ examples/chessbot.js
@@ -1,13 +1,34 @@
+// Sets up a chess board for a room with this scene: https://hubs.mozilla.com/scenes/qEa4XwA
+
 const {HubsBot} = require('../index.js')
+
+PIECE_MODELS = {
+  black: {
+    pawn: "https://uploads-prod.reticulum.io/files/f1d4edc9-2215-436c-9c27-5441f4463062.glb",
+    rook: "https://uploads-prod.reticulum.io/files/7cc722a5-aa8a-4f5b-be24-0401cefde6b0.glb",
+    knight: "https://uploads-prod.reticulum.io/files/9b2c9b0c-e4a3-46dd-a7ba-c4edfc6de734.glb",
+    bishop: "https://uploads-prod.reticulum.io/files/e041d895-6a01-4034-a70e-542d71704cc6.glb",
+    queen: "https://uploads-prod.reticulum.io/files/87cdd853-943c-43aa-afdd-5c2255aa18ec.glb",
+    king: "https://uploads-prod.reticulum.io/files/8bbd0933-970b-45fe-9d2b-31ecfe51de6f.glb"
+  },
+  white: {
+    pawn: "https://uploads-prod.reticulum.io/files/537e3fb5-3692-4b39-8d5d-28f59d1450d2.glb",
+    rook: "https://uploads-prod.reticulum.io/files/590d79bd-3631-485c-9587-e1d5264abbc9.glb",
+    knight: "https://uploads-prod.reticulum.io/files/67b9b82a-2e86-4fe0-9935-b3883f9731ee.glb",
+    bishop: "https://uploads-prod.reticulum.io/files/9d7436ba-3214-4ca8-b24c-f23c4574e3ef.glb",
+    queen: "https://uploads-prod.reticulum.io/files/99764c83-5bae-4317-98a7-3fac40e0ae82.glb",
+    king: "https://uploads-prod.reticulum.io/files/f0dc3e18-8557-43ad-af35-bfe880352592.glb",
+  }
+}
 
 class ChessBot extends HubsBot {
   constructor({...args} = {}) {
     super({name: "Chess Bot", ...args})
   }
-  async dropPawn(position) {
+  async dropPiece(color, piece, position) {
     this.pieces = this.pieces || []
-    this.pieces.push(this.spawnObject({
-      url: "https://uploads-prod.reticulum.io/files/f1d4edc9-2215-436c-9c27-5441f4463062.glb",
+    this.pieces.push(await this.spawnObject({
+      url: PIECE_MODELS[color][piece],
       position,
       dynamic: true,
       gravity: { x: 0, y: -9.8, z: 0 },
@@ -16,19 +37,53 @@ class ChessBot extends HubsBot {
   }
   async populateBoard() {
     console.log("Populating board")
+
+
+    await this.dropPiece("black", "rook", `3.5 1 3`)
+    await this.dropPiece("black", "rook", `${3.5 - 7} 1 3`)
+
+    await this.dropPiece("white", "rook", `3.5 1 -4`)
+    await this.dropPiece("white", "rook", `${3.5 - 7} 1 -4`)
+
+    await this.dropPiece("black", "knight", `2.5 1 3`)
+    await this.dropPiece("black", "knight", `${2.5 - 5} 1 3`)
+
+    await this.dropPiece("white", "knight", `2.5 1 -4`)
+    await this.dropPiece("white", "knight", `${2.5 - 5} 1 -4`)
+
+    await this.dropPiece("black", "bishop", `1.5 1 3`)
+    await this.dropPiece("black", "bishop", `${1.5 - 3} 1 3`)
+
+    await this.dropPiece("white", "bishop", `1.5 1 -4`)
+    await this.dropPiece("white", "bishop", `${1.5 - 3} 1 -4`)
+
+    await this.dropPiece("white", "queen", `0.5 1 -4`)
+    await this.dropPiece("black", "queen", `0.5 1 3`)
+
+    await this.dropPiece("white", "king", `-0.5 1 -4`)
+    await this.dropPiece("black", "king", `-0.5 1 3`)
+
     for (let i = 0; i < 8; ++i)
     {
-      await this.dropPawn(`${3.5 - i} 1 2`)
-      await this.page.waitFor(100)
+      await this.dropPiece("black", "pawn", `${3.5 - i} 1 2`)
+    }
+
+    for (let i = 0; i < 8; ++i)
+    {
+      await this.dropPiece("white", "pawn", `${3.5 - i} 1 -3`)
     }
   }
 
   async runBot(room) {
     await this.catchAndScreenShot(async () => {
       await this.enterRoom(room)
+      await this.page.evaluate(() => {
+        document.querySelector('*[networked-counter]').setAttribute('networked-counter', {max: 100})
+      })
       await this.populateBoard()
+      await this.goTo({x: -5.0, y: 0, z: -1})
     })
   }
 }
 
-new ChessBot({headless: false}).runBot("https://hubs.mozilla.com/NA4yYAL/artistic-snow-spot")
+new ChessBot({headless: true}).runBot("https://hubs.mozilla.com/NA4yYAL/artistic-snow-spot")
